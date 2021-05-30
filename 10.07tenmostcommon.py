1b6ddbef4190289f81426df9796ff39848ff3677
marcoh3mmm <84488222+marcoh3mmm@users.noreply.github.com>
2021-05-29 21:48:08 -0500

    Add files via upload

---
+++ 10.07tenmostcommon.py
@@ -0,0 +1,25 @@
+#Open the File
+fhand = open('romeo.txt')
+counts = dict()
+
+#read the lines
+for line in fhand:
+    #Split the lines
+    words = line.split()
+    #Count the words in the lines and  get them in dictionary
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+#Append the Key And Values in different order
+lst = list()
+for key, val in counts.items():
+    newtup = ( val,key)
+    lst.append(newtup)
+
+#Sort the list  from highest to lowest
+lst = sorted(lst, reverse= True)
+
+#Print until the ten Highest Values
+for val,key in lst[:10] :
+    print(key, val)
+
